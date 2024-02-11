68edc3fc519cfbeae33699e480cce3c7b78e5d67
mahallaDev <157531040+mahallaDev@users.noreply.github.com>
2024-02-11 21:03:13 +0530

    Add files via upload

---
+++ icicibankproduce/src/main/java/com/icicibank/produce/icicibankproduce/IcicibankproduceApplication.java
@@ -0,0 +1,13 @@
+package com.icicibank.produce.icicibankproduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcicibankproduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcicibankproduceApplication.class, args);
+	}
+
+}
